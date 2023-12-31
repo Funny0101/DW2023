82bf21bc561dfb1d640768514b17cced0c697672
BronyaKaslana06 <97924591+BronyaKaslana06@users.noreply.github.com>
2023-12-31 13:10:30 +0800

    Add files via upload
    添加前端界面

---
+++ data-warehouse-frontendFinal/src/layout/components/Navbar.vue
@@ -0,0 +1,139 @@
+<template>
+  <div class="navbar">
+    <hamburger :is-active="sidebar.opened" class="hamburger-container" @toggleClick="toggleSideBar" />
+
+    <breadcrumb class="breadcrumb-container" />
+
+    <div class="right-menu">
+      <el-dropdown class="avatar-container" trigger="click">
+        <div class="avatar-wrapper">
+          <img :src="avatar+'?imageView2/1/w/80/h/80'" class="user-avatar">
+          <i class="el-icon-caret-bottom" />
+        </div>
+        <el-dropdown-menu slot="dropdown" class="user-dropdown">
+          <router-link to="/">
+            <el-dropdown-item>
+              Home
+            </el-dropdown-item>
+          </router-link>
+          <!-- <a target="_blank" href="https://github.com/Baokker/data-warehouse-frontend">
+            <el-dropdown-item>Github</el-dropdown-item>
+          </a>
+          <a target="_blank" href="https://baokker.github.io/2022/12/17/%E6%95%B0%E6%8D%AE%E4%BB%93%E5%BA%93%E8%AF%BE%E7%A8%8B%E9%A1%B9%E7%9B%AE%E7%BB%8F%E9%AA%8C%E5%BF%83%E5%BE%97/">
+            <el-dropdown-item>Docs</el-dropdown-item>
+          </a> -->
+          <el-dropdown-item divided @click.native="logout">
+            <span style="display:block;">Log Out</span>
+          </el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import Breadcrumb from '@/components/Breadcrumb'
+import Hamburger from '@/components/Hamburger'
+
+export default {
+  components: {
+    Breadcrumb,
+    Hamburger
+  },
+  computed: {
+    ...mapGetters([
+      'sidebar',
+      'avatar'
+    ])
+  },
+  methods: {
+    toggleSideBar() {
+      this.$store.dispatch('app/toggleSideBar')
+    },
+    async logout() {
+      await this.$store.dispatch('user/logout')
+      this.$router.push(`/login?redirect=${this.$route.fullPath}`)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+  height: 50px;
+  overflow: hidden;
+  position: relative;
+  background: #fff;
+  box-shadow: 0 1px 4px rgba(0,21,41,.08);
+
+  .hamburger-container {
+    line-height: 46px;
+    height: 100%;
+    float: left;
+    cursor: pointer;
+    transition: background .3s;
+    -webkit-tap-highlight-color:transparent;
+
+    &:hover {
+      background: rgba(0, 0, 0, .025)
+    }
+  }
+
+  .breadcrumb-container {
+    float: left;
+  }
+
+  .right-menu {
+    float: right;
+    height: 100%;
+    line-height: 50px;
+
+    &:focus {
+      outline: none;
+    }
+
+    .right-menu-item {
+      display: inline-block;
+      padding: 0 8px;
+      height: 100%;
+      font-size: 18px;
+      color: #5a5e66;
+      vertical-align: text-bottom;
+
+      &.hover-effect {
+        cursor: pointer;
+        transition: background .3s;
+
+        &:hover {
+          background: rgba(0, 0, 0, .025)
+        }
+      }
+    }
+
+    .avatar-container {
+      margin-right: 30px;
+
+      .avatar-wrapper {
+        margin-top: 5px;
+        position: relative;
+
+        .user-avatar {
+          cursor: pointer;
+          width: 40px;
+          height: 40px;
+          border-radius: 10px;
+        }
+
+        .el-icon-caret-bottom {
+          cursor: pointer;
+          position: absolute;
+          right: -20px;
+          top: 25px;
+          font-size: 12px;
+        }
+      }
+    }
+  }
+}
+</style>
---
+++ data-warehouse-frontendFinal/src/views/test.vue
@@ -0,0 +1,59 @@
+<template>
+  <el-button @click="testpost()">点击发送POST和GET</el-button>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/name-property-casing
+  name: "test",
+  data() {
+    return {};
+  },
+  methods: {
+    testpost() {
+      // post方法
+      this.$axios.post("/post", {
+        data: {
+          title: "harry",
+          date: {
+            year: 2022,
+            month: 11,
+            day: 23
+          },
+          "director": "",
+          actor: "",
+          genre: "",
+          "score": {
+            min: 2,
+            max: 4,
+          },
+          columns: ["title", "asin", "score"],
+        }
+      }).then(res => {
+
+      }).catch(err => {
+
+      });
+
+      // get方法
+      this.$axios.get("/get", {
+        params: {
+          date: {
+            year: 2022,
+            month: 4,
+          }
+        }
+      }).then(res => {
+
+      }).catch(err => {
+
+      })
+    },
+    test() {
+      this.$axios.get("/spark/bytime/year", { params: { year: 2008 } });
+      this.$axios.get("/mysql/bytime/year", { params: { year: 2008 } });
+      this.$axios.get("/neo4j/bytime/year", { params: { year: 2008 } });
+    },
+  }
+}
+</script>
